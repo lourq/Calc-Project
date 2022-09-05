b614e5265efff59728b21a4db61385c2eab37cc1
L <you@example.com>
2022-09-05 14:54:19 +0300

    as asdasd

--- CalcProject/Program.cs
+++ CalcProject/Program.cs
@@ -1,3 +1,7 @@
 ﻿new CalcProject.App.Calc().Run();
 
-// Hello from VS    
+<<<<<<< HEAD
+// Hello from VS    asdasd 
+=======
+// Hello from GitHub
+>>>>>>> e85bb7be11a565e1e33b37fafb2b093fbb266edd
