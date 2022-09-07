e70f1b50e930c0757bb60aa605772854e3f95e94
sarievgeo <75241201+sarievgeo@users.noreply.github.com>
2022-09-07 21:23:02 +0300

    Update AppTest.cs
    Adding Test Method RomanNumberParseN.

--- TestProject1/AppTest.cs
+++ TestProject1/AppTest.cs
@@ -23,5 +23,14 @@ namespace TestProject1
             Assert.AreEqual(RomanNumber.Parse("V") , 5 , "V == 5");
             Assert.AreEqual(RomanNumber.Parse("XV") , 15);
         }
-    }
+        
+        [TestMethod]
+        public void RomanNumberParseN()
+        {
+            Assert.AreEqual("Invalid char N", Assert.ThrowsException<ArgumentException>(() => RomanNumber.Parse("XN")).Message);
+            Assert.AreEqual("Invalid char N", Assert.ThrowsException<ArgumentException>(() => RomanNumber.Parse("XNX")).Message);
+            Assert.AreEqual("Invalid char N", Assert.ThrowsException<ArgumentException>(() => RomanNumber.Parse("NX")).Message);
+            Assert.AreEqual("Invalid char N", Assert.ThrowsException<ArgumentException>(() => RomanNumber.Parse("NVII")).Message);
+            Assert.AreEqual("Invalid char N", Assert.ThrowsException<ArgumentException>(() => RomanNumber.Parse("NN")).Message);
+        }
 }
