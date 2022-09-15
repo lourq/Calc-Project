764e1c2a090567771da872e8989133d7cab25b38
Your Name <you@example.com>
2022-09-15 03:25:54 +0300

    update AppTest.cs , RomanNumber.cs

--- CalcProject/App/RomanNumber.cs
+++ CalcProject/App/RomanNumber.cs
@@ -14,7 +14,7 @@ namespace CalcProject.App
     {
         #region Data
         
-        private static readonly Dictionary<char , int> digits = new ()
+        private static readonly Dictionary<char , int> Digits = new ()
         {
             {'I',1},                                                                                                                           
             {'V',5},                                                                                                                           
@@ -33,7 +33,7 @@ namespace CalcProject.App
 
         public RomanNumber(int number = 0)
         {
-            Value = number;
+            this.Value = number;
         }
 
         #endregion
@@ -47,22 +47,59 @@ namespace CalcProject.App
 
         public static RomanNumber Add(RomanNumber num1, int num2)
         {
+            if (num1 is null)
+                throw new ArgumentException("Empty obj");
             return new RomanNumber(num1.Value + num2);
         }
 
         public static RomanNumber Add(string num1, string num2)
         {
-            return new RomanNumber(Parse(num1) + Parse(num2));
+            return new RomanNumber(Parse(num1) + RomanNumber.Parse(num2));
         }
 
         public static RomanNumber Add(RomanNumber num1, string num2)
         {
+            if (num1 is null)
+                throw new ArgumentException("Empty obj");
             return new RomanNumber(num1.Value + Parse(num2));
         }
 
+        public static RomanNumber Add(string num1, RomanNumber num2)
+        {
+            if (num2 is null)
+                throw new ArgumentException("Empty obj");
+            return new RomanNumber(Parse(num1) + num2.Value);
+        }
         public static RomanNumber Add(RomanNumber num1, RomanNumber num2)
         {
+            if (num1  is null || num2 is null)
+            {
+                throw new ArgumentException("Empty obj");
+            }
             return new RomanNumber(num1.Value + num2.Value);
+        }
+
+        public static RomanNumber Add(int num1, string num2)
+        {
+            return new RomanNumber(num1 + Parse(num2));
+        }
+        
+        public RomanNumber Add(int num)
+        {
+            return new RomanNumber(this.Value + num);
+        }
+    
+        public RomanNumber Add(String num)
+        {
+            return new(this.Value + Parse(num));
+        }
+        
+        public RomanNumber Add(RomanNumber num)
+        {
+            if (num is null)
+                throw new ArgumentException("Empty obj");
+            
+            return new(this.Value + num.Value);
         }
         
         #endregion
@@ -98,34 +135,46 @@ namespace CalcProject.App
         #endregion
 
         #region Parse String to int method
-        public static int Parse(String str)
+        public static int Parse(String str) 
         {
+
+            #region var
+
+            var sum = 0;
+            bool negNum = false;
+
+            #endregion
+            
             if (str == null)
                 throw new ArgumentNullException();
         
             if (str == string.Empty)
                 throw new ArgumentException("Empty string not allowed");
             
+            if (str.StartsWith('-') && str.Length > 1)
+            {
+                negNum = true;
+                str = str[1..];
+            }
+            
             foreach (var inputSymbol in str)
             {
-                if (!digits.ContainsKey(inputSymbol))
+                if (!Digits.ContainsKey(inputSymbol))
                     throw new ArgumentException($"Invalid input data: {inputSymbol}");
             }
-
             
             #region Parse to Int
 
             if (str.Length <= 1)
             {
-                return digits[Char.Parse(str)];
+                return Digits[Char.Parse(str)];
             }
             
-            var sum = 0;
 
             for (int i = 0; i < str.Length - 1; i++)
             {
-                var number = digits[str[i]];
-                var nextNumber = digits[str[i + 1]];
+                var number = Digits[str[i]];
+                var nextNumber = Digits[str[i + 1]];
 
                 if (number > nextNumber || number == nextNumber)
                 {
@@ -137,11 +186,11 @@ namespace CalcProject.App
                 }
             }
 
-            sum += digits[str[str.Length - 1]];
+            sum += Digits[str[^1]];
             
             #endregion
-            
-            return sum;
+
+            return negNum ? -sum : sum;
         }
         #endregion
     }
--- TestProject1/AppTest.cs
+++ TestProject1/AppTest.cs
@@ -105,32 +105,94 @@ namespace TestProject1
         {
             RomanNumber num = new();
             for (int n = 1; n <= 2022; ++n)
-            {   
+            {
                 num.Value = n;
                 Assert.AreEqual(n, RomanNumber.Parse(num.ToString()));
             }
         }
-        
-    [TestMethod]
-    public void ToStringNegative()
-    {
-        RomanNumber rn = new() { Value = -10 };
-        Assert.AreEqual("-X", rn.ToString());
-        rn.Value = -90;
-        Assert.AreEqual("-XC", rn.ToString());
+
+        [TestMethod]
+        public void ToStringNegative()
+        {
+            RomanNumber rn = new() { Value = -10 };
+            Assert.AreEqual("-X", rn.ToString());
+            rn.Value = -90;
+            Assert.AreEqual("-XC", rn.ToString());
+        }
+
+        [TestClass]
+        public class OperationsTest
+        {
+            [TestMethod]
+            public void AddRNTest()
+            {
+                RomanNumber rn2 = new(2);
+                RomanNumber rn5 = new(5);
+                RomanNumber rn7 = new(7);
+                RomanNumber rn10 = new() { Value = 10 };
+                RomanNumber rn_5 = new() { Value = -5 };
+                RomanNumber rn_7 = new() { Value = -7 };
+
+                Assert.AreEqual(9, rn2.Add(rn7).Value);
+                Assert.AreEqual(20, rn10.Add(rn10).Value);
+                Assert.AreEqual(5, rn10.Add(rn_5).Value);
+                Assert.AreEqual(3, rn10.Add(rn_7).Value);
+                Assert.AreEqual(10, rn5.Add(rn5).Value);
+                Assert.AreEqual(7, rn7.Add(new RomanNumber(0)).Value);
+                Assert.AreEqual(5, rn5.Add(new RomanNumber()).Value);
+                Assert.AreEqual(25, rn5.Add(new RomanNumber(20)).Value);
+                Assert.AreEqual(6, rn5.Add(new RomanNumber(1)).Value);
+                Assert.AreEqual(19, rn10.Add(new RomanNumber(9)).Value);
+                Assert.AreEqual(-5, rn5.Add(new RomanNumber(-10)).Value);
+                Assert.AreEqual(rn7, rn2.Add(rn5));
+                Assert.AreEqual(rn_5, rn_7.Add(rn2));
+                Assert.AreEqual("XVII", rn7.Add(rn10).ToString());
+                Assert.AreEqual("III", rn_7.Add(rn10).ToString());
+                Assert.AreEqual("-V", rn_7.Add(rn2).ToString());
+                Assert.AreEqual("-XII", rn_7.Add(rn_5).ToString());
+
+                Assert.ThrowsException<ArgumentNullException>(() => rn2.Add((RomanNumber)null!));
+
+            }
+
+            [TestMethod]
+            public void AddValueTest()
+            {
+                var rn = new RomanNumber(10);
+                Assert.AreEqual(20, rn.Add(10).Value);
+                Assert.AreEqual("V", rn.Add(-5).ToString());
+                Assert.AreEqual(rn, rn.Add(0));
+            }
+
+            [TestMethod]
+            public void AddStringTest()
+            {
+                var rn = new RomanNumber(10);
+                Assert.AreEqual(30, rn.Add("XX").Value);
+                Assert.AreEqual("-XL", rn.Add("-L").ToString());
+                Assert.AreEqual(rn, rn.Add("N"));
+
+                Assert.ThrowsException<ArgumentException>(() => rn.Add(""));
+                Assert.ThrowsException<ArgumentException>(() => rn.Add("-"));
+                Assert.ThrowsException<ArgumentException>(() => rn.Add("10"));
+                Assert.ThrowsException<ArgumentNullException>(() => rn.Add((String)null!));
+            }
+
+            [TestMethod]
+            public void AddStaticTest()
+            {
+                RomanNumber rn5 = RomanNumber.Add(2, 3);
+                RomanNumber rn8 = RomanNumber.Add(rn5, 3);
+                RomanNumber rn10 = RomanNumber.Add("I", "IX");
+                RomanNumber rn9 = RomanNumber.Add(rn5, "IV");
+                RomanNumber rn13 = RomanNumber.Add(rn5, rn8);
+                Assert.AreEqual(5, rn5.Value);
+                Assert.AreEqual(8, rn8.Value);
+                Assert.AreEqual(9, rn9.Value);
+                Assert.AreEqual(10, rn10.Value);
+                Assert.AreEqual(13, rn13.Value);
+            }
+        }
     }
-    [TestMethod]
-    public void AddStaticTest()
-    {
-        RomanNumber rn5  = RomanNumber.Add(2, 3);
-        RomanNumber rn8  = RomanNumber.Add(rn5, 3);
-        RomanNumber rn10 = RomanNumber.Add("I", "IX");
-        RomanNumber rn9  = RomanNumber.Add(rn5, "IV");
-        RomanNumber rn13 = RomanNumber.Add(rn5, rn8);
-        // Задание: составить утверждения для тестирования RomanNumber.Add
-        // Реализовать RomanNumber.Add и необходимые перегрузки
-        // Расширить тесты с учетом исключительных ситуаций
-        // ! В стиле ХР не делаем лишнего
-    }
-    }
+
 }
